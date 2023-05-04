168c47a92dcc52da36e9f9a16b1d28bdeee24401
venugk81 <venu.gk81@gmail.com>
2023-05-05 00:05:26 +0530

    added

--- src/com/framework/TestSingleScript.py
+++ src/com/framework/TestSingleScript.py
@@ -18,9 +18,10 @@ class UpdateTimeEntry(unittest.TestCase):
         print("driver is initialized..")
 
     def tearDown(self):
-        self.driver.close()
-        self.driver.quit()
-        print("driver instance terminated")
+        if self.driver is not None:
+            print("Cleanup of test environment")
+            self.driver.close()
+            self.driver.quit()
 
     def is_element_clickable(self, element, time_out):
         wait = WebDriverWait(self.driver, time_out)
@@ -31,12 +32,15 @@ class UpdateTimeEntry(unittest.TestCase):
         return wait.until(expected_conditions.visibility_of_element_located(by, value))
 
     def test_method(self):
-        search_bar = self.is_element_clickable(self.driver.find_element(By.XPATH, self.txt_name), 10)
-        # search_bar = self.driver.find_element(By.XPATH, self.txt_name)
-        search_bar.clear()
-        search_bar.send_keys("getting started with python")
-        print(self.driver.current_url)
-        print("Test method is executed..")
+        try:
+            search_bar = self.is_element_clickable(self.driver.find_element(By.XPATH, self.txt_name), 10)
+            # search_bar = self.driver.find_element(By.XPATH, self.txt_name)
+            search_bar.clear()
+            search_bar.send_keys("getting started with python")
+            print(self.driver.current_url)
+            print("Test method is executed..")
+        except Exception as err:
+            print(err + "- WebPage is not loaded")
 
     if __name__ == '__main__':
         unittest.main()
